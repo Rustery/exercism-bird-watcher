2dba9ba432209337f75ffc50497d4a9f8b40ef18
Rustam Khasanshin <rustery@gmail.com>
2022-06-17 17:49:18 +0500

    initial

---
+++ bird_watcher.go
@@ -0,0 +1,32 @@
+package birdwatcher
+
+// TotalBirdCount return the total bird count by summing
+// the individual day's counts.
+func TotalBirdCount(birdsPerDay []int) int {
+	total := 0
+	for _, v := range birdsPerDay {
+		total += v
+	}
+	return total
+}
+
+// BirdsInWeek returns the total bird count by summing
+// only the items belonging to the given week.
+func BirdsInWeek(birdsPerDay []int, week int) int {
+	total := 0
+	for _, v := range birdsPerDay[week*7-7 : week*7] {
+		total += v
+	}
+	return total
+}
+
+// FixBirdCountLog returns the bird counts after correcting
+// the bird counts for alternate days.
+func FixBirdCountLog(birdsPerDay []int) []int {
+	for i := range birdsPerDay {
+		if i == 0 || (i+2)%2 == 0 {
+			birdsPerDay[i]++
+		}
+	}
+	return birdsPerDay
+}
